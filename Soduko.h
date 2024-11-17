9bb79b88d40098f6047d41866c496adec5d6d586
sarahossam-2005 <sarahossam@aucegypt.edu>
2024-11-17 23:16:08 +0200

    Soduko.h complete

--- Soduko.h
+++ Soduko.h
@@ -1,12 +1,46 @@
+#ifndef SUDOKU_H
+#define SUDOKU_H
+
 #include <iostream>
+#include <vector>
+#include <cmath>
+
 using namespace std;
 
-//Soduko Class
-class Sudoko
+
+class Sudoku 
 {
 private:
+    // Size of the Sudoku grid 
+    int gridSize;  
 
+    // Size of each sub-grid 
+    int subGridSize;   
+
+    //2D vector 
+    vector<vector<int>> sudokuGrid;  
+
+    // Helper function
+    // Checks if it's safe to place num into a position
+    bool isAvailable(int row, int col, int num);
+
+public:
+
+    // Constructor
+    Sudoku(int size);
+
+    // Takes input for the grid from the user
+    void inputGrid();
+
+    // Displays the Sudoku grid
+    void printGrid();
+
+    // Solves the Sudoku using backtracking
+    bool solve();
+
+    // Backtracking to be called in the solve function
+    bool backtrack();
 };
-/*
 
-*/
+#endif // SUDOKU_H
+
